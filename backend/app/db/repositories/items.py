fe5b1d759940416509d285af529910eb81363436
Simone Sava <ssava@users.noreply.github.com>
2023-01-26 20:34:29 +0100

    Added support for title filtering (#1)

--- backend/app/db/repositories/items.py
+++ backend/app/db/repositories/items.py
@@ -104,6 +104,7 @@ class ItemsRepository(BaseRepository):  # noqa: WPS214
         self,
         *,
         tag: Optional[str] = None,
+        title: Optional[str] = None,
         seller: Optional[str] = None,
         favorited: Optional[str] = None,
         limit: int = 20,
@@ -154,6 +155,16 @@ class ItemsRepository(BaseRepository):  # noqa: WPS214
                         tags_table.tag,
                     )
                 ),
+            )
+            # fmt: on
+
+        if title:
+            query_params.append('%{toFind}%'.format(toFind = title))
+            query_params_count += 1
+
+            # fmt: off
+            query = query.where(
+                items.title.like(Parameter(query_params_count))
             )
             # fmt: on
 
